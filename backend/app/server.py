7a9be2095b00157cfe1152242a59327db3ffc291
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-15 17:12:38 -0300

    fix: remove exception handler

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,21 +1,11 @@
 from typing import Annotated
 
 from fastapi import Body, FastAPI
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
 
 from app import model
 from app.schemas import GenerateQuestionsParams
 
 app = FastAPI()
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=400,
-        content={"message": str(exc)},
-    )
 
 
 @app.post("/questions")
