1693523510db0964cb7193cdb610e1e415947c98
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-15 17:24:48 -0300

    feat: refine model prompt

--- backend/app/prompter.py
+++ backend/app/prompter.py
@@ -4,6 +4,18 @@ from app.schemas import GenerateQuestionsParams
 def get_prompt(params: GenerateQuestionsParams):
     PROMPT = f"""
         You should give {params.number_of_questions} random versions of the
-        following text delimited by $: ${params.original_text}$
+        following text delimited by the char $, this is the text: ${params.original_text}$
+
+        Remove the delimiter char $ from the generated text.
+        You can change the words and the context of the provided text.
+
+        I want the output to be in the following format:
+        generated_text1
+        generated_text2
+        generated_text3
+        ...
+        generated_textn
+
+        So you only should give me {params.number_of_questions} lines of generated text.
     """
     return PROMPT
