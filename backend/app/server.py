02e89020e91987475982eabd0f47c28d95dc6f0a
mauricio-monte <mauricio.monte@ccc.ufcg.edu.br>
2023-10-17 15:30:23 -0300

    Add CORS configuration

--- backend/app/server.py
+++ backend/app/server.py
@@ -4,9 +4,21 @@ from fastapi import Body, FastAPI
 
 from app import model
 from app.schemas import GeneratedQuestionsResponse, GenerateQuestionsParams
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/questions", response_model=GeneratedQuestionsResponse)
 def generate_questions(params: Annotated[GenerateQuestionsParams, Body]):
