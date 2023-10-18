d4f8e2be0fce46d6213b19646368cfe7780a311c
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-17 23:48:44 -0300

    feat: improve prompt from v4

--- backend/app/prompter.py
+++ backend/app/prompter.py
@@ -5,24 +5,30 @@ def get_prompt(params: GenerateQuestionsParams):
     PROMPT = f"""
         You are a teacher and want to generate different questions with the same solution for your students.
 
-        Your task is generate alternatives versions of the original programming question delimited by triple backticks.
-        To solve your task, do the following:
-        - Generate {params.number_of_questions} new programming questions with completely different contexts
-        in which they have the same solution from the original programming question delimited.
+        Your task is to generate {params.number_of_questions} new programming questions with completely different contexts
+        in which they have the same solution from the original programming question delimited. Everything should be changed
+        except information relevant to the algorithmic solution.
+
+        To solve your task, change the following, if it makes sense:
+        1. all the characters, names, type of beings, places, et.
+        2. The actions, use different verbs, adverbs, etc.
+        3. The context, use different places, situations, etc.
+        4. The time, use different time periods, etc.
+        5. The objects, use different objects, etc.
 
         Original programming question:
         ```{params.original_text}```
 
-        You only should return the generated question texts, nothing more, so remove the delimiter chars.
-        For the output format, separate the generated versions by six equal signs in the following format:
+        For the output format, don't add nothing more to your output, just
+        separate the generated versions by six equal signs in the following format:
 
-        <generated question 1>
+        <text from generated question 1>
         ======
-        <generated question 2>
+        <text from generated question 2>
         ======
         ...
         ======
-        <generated question N>
+        <text from generated question N>
 
     """
     return PROMPT
