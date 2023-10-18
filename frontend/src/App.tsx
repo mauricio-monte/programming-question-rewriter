15ac4b76bd8454cce2fd1ca61dbda02c8c8528e8
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-18 00:32:47 -0300

    feat: add logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import QuestionSubmissionForm from "./components/QuestionSubmissionForm";
 import QuestionVariationsDisplay from "./components/QuestionVariationsDisplay";
 import { QuestionsVariationsProvider } from "./context/QuestionsVariationsContext";
+import logoPNG from "./assets/logo.png";
 
 function App() {
   return (
-    <div className="flex flex-col items-center min-w-full min-h-screen">
-      <h1 className="text-center text-2xl">Programming Question Rewriter</h1>
+    <div className="flex flex-col items-center min-w-full min-h-screen gap-16 pt-10">
+      <header className="flex flex-col justify-center items-center">
+        <img
+          className="w-[100px]"
+          src={logoPNG}
+          alt="Programming Question Rewriter Logo"
+        />
+
+        <h1 className="text-center text-2xl">Programming Question Rewriter</h1>
+      </header>
+
       <QuestionsVariationsProvider>
         <QuestionSubmissionForm></QuestionSubmissionForm>
         <QuestionVariationsDisplay></QuestionVariationsDisplay>
